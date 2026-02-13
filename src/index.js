bc2564c856600e6d9f07d5a9a9f831bba4615424
ASuarez18 <alejandro.sg.1825@gmail.com>
2026-02-12 20:40:00 -0600

    White box testing planned and recorded on TEST-PLAN

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
-const path = require('path');
-const readline = require('readline');
-const chalk = require('chalk');
-const Table = require('cli-table3');
+import fs from 'fs';
+import path from 'path';
+import readline from 'readline';
+import chalk from 'chalk';
+import Table from 'cli-table3';
 
 const dataPath = path.resolve(process.cwd(), 'bank-data.json');
 let data = { accounts: [] };
@@ -413,11 +413,11 @@ async function transferFunds() {
 
     data.accounts.push(toAccount);
   } else {
-    if (!toId.trim().endsWith('7')) {
+    if (!toId.trim().endsWith('7')) { // TODO: Fix bug
       toAccount.balance += amount;
     }
 
-    if (amount <= 500) {
+    if (amount <= 500) { // TODO: Fix bug
       toAccount.transactions.push({
         type: 'TRANSFER_IN',
         amount,
